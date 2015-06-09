0c325ca52f7d1ba4dc2fa9c62c9278b8a527e21b
Sarwan <Sarwan.net@gmail.com>
2015-06-09 17:33:45 +0600

    upload

---
+++ Default.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Title="تور بەت مەزمۇنىنى ئېلىش" Language="C#" MasterPageFile="~/Common/Public.master" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="cpheader" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+    <table cellpadding="0" cellspacing="0" class="BodyTable">
+        <tr>
+            <td>
+                تور بەت مەزمۇنىنى ئېلىش<br />
+                grab web page content
+            </td>
+        </tr>
+        <tr>
+            <td>
+                تور بەت ئادرىسى(URL):
+                <asp:TextBox ID="txtWebPageUrl" runat="server" 
+                    Width="420px" style=" text-align:left; direction:ltr;"></asp:TextBox>
+                <asp:Button ID="btnGetContent" runat="server" Text="مەزمۇن ئېلىش" 
+                    onclick="btnGetContent_Click" />
+            </td>
+        </tr>
+        <tr>
+            <td style=" border: thin solid #3399FF; direction:rtl; text-align:right;">
+                <asp:Literal ID="ltrlContent" runat="server"></asp:Literal>
+            </td>
+        </tr>
+    </table>
+</asp:Content>
+
---
+++ Default.aspx.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!Page.IsPostBack)
+        {
+            string strDescription = "<strong>مەزكۇر مۇلازىمەت ئارقىلىق سىز تور بىكەتلەردىكى خەۋەر، ماقالىلەرنىڭ مەزمۇن قىسمىنى ئايرىپ ئالالايسىز.</strong>"
+                +"<br/>"
+                +"<b>ئادرىس كىرگۈزگەندە بىلوگ، خەۋەر بەتلىرىنىڭ باش بېتىنى ئەمەس ئايرىم مەزمۇن بېتىنىڭ ئادرىسىنى كىرگۈزۈڭ.</b>"
+                +"<br/>"
+                +"بۇ مۇلازىمەت سىناق ئۈستىدە. خاتالىقلار تۈزىتىلىۋاتىدۇ.";
+            ltrlContent.Text = strDescription;
+        }
+    }
+    protected void btnGetContent_Click(object sender, EventArgs e)
+    {
+        Uri strUrl;
+        if(Uri.TryCreate(txtWebPageUrl.Text,UriKind.Absolute, out strUrl)==false)
+        {
+            ltrlContent.Text = "<i>تور بەت ئادرىسىدا مەسىلە بار.</i>";
+            return;
+        }
+        if (String.IsNullOrEmpty(txtWebPageUrl.Text.Trim()))
+        {
+            ltrlContent.Text = "<i>مەزمۇنىنى ئالماقچى بولغان تور بەتنىڭ ئادرىسىنى چاپلاڭ.</i>";
+            return;
+        }
+        try
+        {
+            WawaSoft.Search.GetMainContentHelper gmc = new WawaSoft.Search.GetMainContentHelper();
+            string strText = WawaSoft.Search.GetMainContentHelper.getDataFromUrl(txtWebPageUrl.Text);
+            ltrlContent.Text = WawaSoft.Search.GetMainContentHelper.GetMainContent(strText).Replace("\n", "<br/>");
+        }
+        catch (Exception ex)
+        {
+            ltrlContent.Text = "خاتالىق كۆرۈلدى.";
+        }
+
+    }
+}
